02eb8c2c5a04f45bd36b84f759e09a71d5266e0c
NAYELI MELISSA URRUTIA ORELLANA <nurrutiao@miumg.edu.gt>
2024-02-24 10:02:02 -0600

    Agregar archivos de proyecto.

---
+++ TEJERCICIOS EN CALSE 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace TEJERCICIOS_EN_CALSE_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROGRAMA 1
+
+
+            Console.WriteLine(" Los primeros 5 numeros pares son:");
+
+            for (int i = 2; i <= 10; i +=2)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+
+            //PROGRAMA 1.2
+
+          
+
+            int resultado = 0;
+            for(int x = 1; x<=10; x++) 
+            {
+                resultado = resultado + x;
+              
+            }
+            Console.WriteLine(" La suma de los primeros 10 Número es:  " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejercicios 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROGRAMA 2
+            int numero1;
+            int numero2=0;
+
+            Console.WriteLine("2.Ingrese Un Número Negativo si quiere salir del bucle");
+            do
+            {
+
+                Console.WriteLine(" Un Número Positivo");
+                 numero1 = int.Parse(Console.ReadLine());
+                if( numero1>=0)
+                {
+                    numero2 += numero1;
+                }
+
+            } while (numero1 >= 0);
+            Console.WriteLine(" La suma es " + numero2);
+
+            Console.ReadKey();
+
+            //PROGRAMA 2.1
+        }
+    }
+}
